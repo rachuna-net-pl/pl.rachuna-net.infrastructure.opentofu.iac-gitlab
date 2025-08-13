eb7939dcdc6707c9575c8b395d62b496f5c113a7
Maciej Rachuna <rachuna.maciej@gmail.com>
2025-08-13 20:18:31 +0000

    feat: Utworzenie repozytorium pl.rachuna-net/cicd/gitlab-profile

---
+++ pl.rachuna-net/cicd/gitlab-profile.tf
@@ -0,0 +1,14 @@
+
+module "gitlab-profile" {
+  source = "git@gitlab.com:pl.rachuna-net/infrastructure/opentofu/modules/gitlab-project.git?ref=v1.0.0"
+
+  name         = "gitlab-profile"
+  description  = "Dokumentacja techniczna dla procesów CI/CD"
+  visibility   = "public"
+  tags         = ["gitlab-profile"]
+  parent_group = local.parent_name
+  project_type = "gitlab-profile"
+
+  # sonarqube
+  is_enabled_sonarqube = false
+}
